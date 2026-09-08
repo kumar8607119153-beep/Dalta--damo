0091e4d4fb7cd80245f7cdd08771e5540d018f29
Sanjay Kumar <kumar8607119153@gmail.com>
2026-09-08 16:17:29 +0530

    Fix assignment of locked_entry in live_dashboard.py

--- live_dashboard.py
+++ live_dashboard.py
@@ -422,7 +422,7 @@ if "locked_entry" not in st.session_state or direction != st.session_state.get("
         
     st.session_state["last_direction"] = direction
 
-entry_display = st.session_state["locked_entry"] = hl2_base
+entry_display = st.session_state["locked_entry"]
         
 t1 = st.session_state["locked_t1"]
 t2 = st.session_state["locked_t2"]
