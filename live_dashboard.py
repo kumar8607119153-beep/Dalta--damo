15a03ff17c22479e02785718943fbc58c0a51888
Sanjay Kumar <kumar8607119153@gmail.com>
2026-09-08 17:52:15 +0530

    Fix new_entry assignment for last close price

--- live_dashboard.py
+++ live_dashboard.py
@@ -497,7 +497,7 @@ if demo_signal is not None:
 
         # New Entry = SuperTrend signal candle CLOSE
 new_entry = float(demo_last["close"])
-          new_entry = float(last["close"])
+        new_entry = float(last["close"])
 
         if demo_signal == "BUY":
             tp1 = new_entry + 300
